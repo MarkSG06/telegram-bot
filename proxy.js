b2823fa41b49c9dd200db86582f90ccc870bfc92
MarkSG06 <markwebsdesigns@hotmail.com>
2025-07-30 12:29:05 +0200

    Production

--- proxy.js
+++ proxy.js
@@ -4,7 +4,7 @@ const app = express();
 
 const options = {
   target: 'http://127.0.0.1:8080', 
-  cookieDomainRewrite: 'dev-telegram-bot.com', 
+  cookieDomainRewrite: 'dev-youthing.com', 
   changeOrigin: true,
   logLevel: 'debug',
   onProxyReq: function(proxyReq, req, res) {
